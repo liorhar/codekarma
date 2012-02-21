8273c0ace04a3e753296e81d95890a13d8604cd5
Lior Harel <harel.lior@gmail.com>
2012-02-21 19:05:26 +0200

    rename flaskr to codekarma

--- codekarma.py
+++ codekarma.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-    Flaskr
+    Code Karma - a dev group code analysis web application
     ~~~~~~
 
-    A microblog example application written as Flask tutorial with
-    Flask and sqlite3.
-
-    :copyright: (c) 2010 by Armin Ronacher.
-    :license: BSD, see LICENSE for more details.
 """
 from __future__ import with_statement
 from sqlite3 import dbapi2 as sqlite3
@@ -16,7 +11,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
 # configuration
-DATABASE = '/tmp/flaskr.db'
+DATABASE = '/tmp/codekarma.db'
 DEBUG = True
 SECRET_KEY = 'kenshooooooo123'
 USERNAME = 'admin'
@@ -25,7 +20,7 @@ PASSWORD = 'default'
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.from_envvar('CODEKARMA_SETTINGS', silent=True)
 
 
 def connect_db():
@@ -56,14 +51,14 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
+    cur = g.db.execute('select author, time, message, score from commits order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    g.db.execute('insert into entries (title, text) values (?, ?)',
+    g.db.execute('insert into commits (title, text) values (?, ?)',
                  [request.form['title'], request.form['text']])
     g.db.commit()
     flash('New entry was successfully posted')
---
+++ codekarma_tests.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+
+    Tests the codekarma application.
+
+"""
+import os
+import codekarma
+import unittest
+import tempfile
+
+
+class CodeKarmaTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """Before each test, set up a blank database"""
+        self.db_fd, codekarma.app.config['DATABASE'] = tempfile.mkstemp()
+        codekarma.app.config['TESTING'] = True
+        self.app = codekarma.app.test_client()
+        codekarma.init_db()
+
+    def tearDown(self):
+        """Get rid of the database again after each test."""
+        os.close(self.db_fd)
+        os.unlink(codekarma.app.config['DATABASE'])
+
+    # testing functions
+
+    def test_empty_db(self):
+        """Start with a blank database."""
+        rv = self.app.get('/')
+        assert 'No entries here so far' in rv.data
+
+
+    def test_messages(self):
+        """Test that messages work"""
+        rv = self.app.post('/add', data=dict(
+            title='<Hello>',
+            text='<strong>HTML</strong> allowed here'
+        ), follow_redirects=True)
+        assert 'No entries here so far' not in rv.data
+        assert '&lt;Hello&gt;' in rv.data
+        assert '<strong>HTML</strong> allowed here' in rv.data
+
+
+if __name__ == '__main__':
+    unittest.main()
