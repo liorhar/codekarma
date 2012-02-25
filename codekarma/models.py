4d96cde8685fa459964078f426cf89adbcf5efb9
Lior Harel <harel.liior@gmail.com>
2012-02-25 19:24:59 +0200

    implement get and update cleanups

--- codekarma/models.py
+++ codekarma/models.py
@@ -1,16 +1,18 @@
-from sqlalchemy import Column, Integer, String, DateTime
-from codekarma.database import Base
+from sqlalchemy import Column, Integer, String, DateTime, func
+from codekarma.database import Base, db_session
+from datetime import datetime
 
 
 class Cleanup(Base):
     __tablename__ = 'cleanups'
-    id = Column(Integer, primary_key=True)
+    revision = Column(Integer, primary_key=True)
     author = Column(String(50), nullable=False)
     message = Column(String(300), nullable=False)
     score = Column(Integer, default=0)
     time = Column(DateTime, nullable=False)
 
-    def __init__(self, author, message, score, time):
+    def __init__(self, revision, author, message, score, time):
+        self.revision = revision
         self.author = author
         self.message = message
         self.score = score
@@ -18,3 +20,14 @@ class Cleanup(Base):
 
     def __repr__(self):
         return '%s - %s - score: %s' % (self.author, self.time, self.score)
+
+
+def get_latest_cleanups():
+    revision = get_latest_revision()
+    print revision
+    revision += 1
+    return [Cleanup(revision , 'liorh',
+        'stam %s' % revision, revision % 5, datetime.now())]
+
+def get_latest_revision():
+    return db_session.query(Cleanup.revision).order_by(Cleanup.revision.desc()).limit(1).one()[0]
--- codekarma/views.py
+++ codekarma/views.py
@@ -1,6 +1,6 @@
 from codekarma import app
 from database import db_session
-from models import Cleanup
+from models import Cleanup, get_latest_cleanups
 from flask import jsonify
 
 
@@ -12,10 +12,20 @@ def teardown_request(exception):
 
 @app.route('/api/cleanups')
 def get_cleanups():
-    cleanups = Cleanup.query.all()
-    return jsonify(cleanups)
+    cleanups = Cleanup.query.order_by(Cleanup.revision.desc()).limit(20)
+    return __jsonify_cleanups(cleanups)
 
 
 @app.route('/api/cleanups', methods=['POST'])
 def update_cleanups():
-    return "ok"
+    cleanups = get_latest_cleanups()
+    for c in cleanups:
+        db_session.add(c)
+    db_session.commit()
+    return __jsonify_cleanups(cleanups)
+
+
+def __jsonify_cleanups(cleanups):
+    j = [dict(author=c.author, message=c.message,
+          score=c.score, time=c.time.isoformat()) for c in cleanups]
+    return jsonify(results=j)
