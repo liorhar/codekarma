33b8945e7f09cae9c774e7e25779572d752dcde5
Lior Harel <lior@liorh-desktop.(none)>
2012-03-08 15:34:46 +0200

    remove unneeded file

--- schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists commits;
-create table commits (
-  id integer primary key autoincrement,
-  author string not null,
-  message string not null,
-  score integer not null,
-  time timestamp not null
-);
